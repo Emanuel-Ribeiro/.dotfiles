7837ab5be1803a4a0c5cd832376607881e7cf1a4
Emanuel Ribeiro <lima.ribeiro.emanuel@gmail.com>
2024-07-02 23:12:17 -0300

    fixing obsidian path

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,18 +1,8 @@
-local is_mac = package.config:sub(1,1) == "/"
-
-local notes_path
-if is_mac then
-  notes_path = "/Users/toor/Obsidian/Notes"
-else
-  notes_path = "/home/toor/Obsidian/notes"
-end
-
 require("obsidian").setup({
   workspaces = {
     {
       name = "Notes",
-      path = notes_path,
+      path = "~/Obsidian/Notes",
     },
   },
 })
-
